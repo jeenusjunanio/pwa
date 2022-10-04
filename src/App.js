52d2518a0a68ac7ba2362192ddbfd607c423645c
jeenusjunanio <jeenussvd@gmail.com>
2022-10-04 14:35:31 +0400

    adding the cache api

--- src/App.js
+++ src/App.js
@@ -17,16 +17,70 @@ const Template = (props) => (
       {props.title}:
     </p>
     <ul className={props.status}>
-        <li>Task 1</li>
-        <li>Task 2</li>
+        <li onClick={()=> saveToken('sRKWQu6hCJgR25lslcf5s12FFVau0ugi')} id="task_1">Task 1</li>
+        <li onClick={displayCachedToken} id="task_2">Task 2</li>
         <li>Task 3</li>
     </ul>
   </div>
 );
 
+const CACHE_NAME = "auth";
+const TOKEN_KEY = "token";
+const FAKE_TOKEN = "sRKWQu6hCJgR25lslcf5s12FFVau0ugi";
+
+// Cache Storage was designed for caching
+// network requests with service workers,
+// mainly to make PWAs work offline.
+// You can give it any value you want in this case.
+const FAKE_ENDPOINT = "/fake-endpoint";
+
+const saveToken = async (token) => {
+  console.log(token);
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    const responseBody = JSON.stringify({
+      [TOKEN_KEY]: token
+    });
+    const response = new Response(responseBody);
+    await cache.put(FAKE_ENDPOINT, response);
+    alert("Token saved! 🎉 " +token);
+  } catch (error) {
+    // It's up to you how you resolve the error
+    alert("saveToken error:"+ { error });
+  }
+};
+
+const getToken = async () => {
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    const response = await cache.match(FAKE_ENDPOINT);
+
+    if (!response) {
+      return null;
+    }
+
+    const responseBody = await response.json();
+    return responseBody[TOKEN_KEY];
+  } catch (error) {
+    // Gotta catch 'em all
+    console.log("getToken error:", { error });
+  }
+};
+
+const displayCachedToken = async () => {
+  const cachedToken = await getToken();
+  alert('token: '+cachedToken);
+};
+
+// Uncomment the line below to save the fake token
+// saveToken(FAKE_TOKEN);
+
+// displayCachedToken();
 
 
 class App extends Component {
+
+  
   render() {
     return (
       <BrowserRouter>
